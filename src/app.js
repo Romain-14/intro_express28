21f6b086582008a9ee8778a75c334bdeb0fec352
Romain-14 <romain14fmsn.com>
2023-08-28 11:42:14 +0200

    add dynamic data with ejs

--- src/app.js
+++ src/app.js
@@ -5,6 +5,20 @@ const express = require("express");
 const app = express();
 const PORT = 9000;
 
+// un objet pour afficher dynamiquement dans la vue
+const products = [
+    {
+        id: 1,
+        label: "short",
+        content: "magnifique short bleu avec des petites fleurs ambiance Hawai",
+    },
+    {
+        id: 2,
+        label: "télé",
+        content: "superbe écran cathodique du grand-père",
+    },
+]
+
 // configurer le chemin des vues, quand la méthode render demande un affichage d'un fichier c'est de ce chemin qu'il va se servir
 app.set("views", "./src/views");
 // configurer le type de fichier utiliser par le moteur de rendu
@@ -19,7 +33,8 @@ app.use(express.static("public"));
 // on cherche à "obtenir" une url spécifique ( un endpoint) ensuite on effectue des instructions en rapport, là en l'occurence quand il reçoit l'url "/" on lui demande de répondre avec un code status 200 (tout va bien) et de rendre (afficher la page home), on ne spécifie pas l'extension du fichier car onj l'a fait lors de la configuration du moteur de rendu ligne 10 !
 app.get("/", (req, res) => {
     console.log("on est sur la home !!!");
-    res.status(200).render("home");
+    // pour envoyer des données dans la vue on passe un objet en tant que second paramètre de la méthode render
+    res.status(200).render("home", {datas : products});
 });
 
 app.get("/shop", (req, res) => {
